826debee1f73caa25e94aa21b33acb9c7df898a6
Arjun Sharma <arjun0941.becse24@chitkara.edu.in>
2026-09-18 01:39:35 +0530

    Update server implementation

--- server.js
+++ server.js
@@ -7,14 +7,22 @@ const PORT = 3000;
 // Allows backend to read JSON sent from frontend
 app.use(express.json());
 
-// Serves files from public folder
-app.use(express.static(path.join(__dirname, "public")));
-
 // Import routes
 const dataRoutes = require("./routes/dataRoutes");
 const authRoutes = require("./routes/authRoutes");
 app.use("/api", dataRoutes);
 app.use("/auth", authRoutes);
+
+const frontendPath = path.join(__dirname, "frontend", "dist");
+app.use(express.static(frontendPath));
+
+app.get("/{*splat}", (req, res) => {
+    if (req.path.startsWith("/api") || req.path.startsWith("/auth")) {
+        return res.status(404).json({ error: "API endpoint not found" });
+    }
+
+    return res.sendFile(path.join(frontendPath, "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
